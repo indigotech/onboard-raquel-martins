8cf8d1270879ca90cac3204e50d69cc60138a336
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-06 15:52:24 -0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { User } from './entity/User';
+import { v4 as uuidv4 } from 'uuid';
 
+//Data store mock
+const users = [
+  {
+    id: '123',
+    name: 'Robert',
+    email: 'robert@email.com',
+    password: 'crespusculo',
+    birthDate: '10-12-1986'
+  },
+  {
+    id: '321',
+    name: 'Kristen',
+    email: 'kristen@email.com',
+    password: 'crespusculo',
+    birthDate: '10-10-1985'
+  }
+];
+
+//Type definition (schema)
 const typeDefs = gql`
   type Query {
     hello: String
+  }
+  type User {
+    id: String
+    name: String
+    email: String
+    password: String
+    birthDate: String
+  }
+  type Mutation {
+    createUser(
+      id: String
+      name: String!
+      email: String!
+      password: String!
+      birthDate: String!
+    ): User!
   }
 `;
 
@@ -11,11 +48,19 @@ const resolvers = {
     hello: () => {
       return 'Hello world!';
     }
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        password: args.password,
+        birthDate: args.birthDate
+      };
+
+      users.push(user);
+      return user;
+    }
   }
 };
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen(4000).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
