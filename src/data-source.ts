1ed4dc8d70e94522a1b8b8b774e2d3d9ba383c1d
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-06 16:00:47 -0300

    integrando e com o banco de dados

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,103 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { User } from './entity/User';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { v4 as uuidv4 } from "uuid";
+import { ApolloServer, gql } from "apollo-server";
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
+  type: "postgres",
+  host: "localhost",
   port: 5432,
-  username: 'raquelmms',
-  password: '123abc',
-  database: 'raquelmms',
+  username: "raquelmms",
+  password: "123abc",
+  database: "raquelmms",
   synchronize: true,
   logging: false,
   entities: [User],
   migrations: [],
-  subscribers: []
+  subscribers: [],
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    const addUser = async ({ id, name, email, password, birthDate }) => {
+      console.log("addUser:", id, name, email, password, birthDate);
+      const response = await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([
+          {
+            id: id,
+            name: name,
+            email: email,
+            password: password,
+            birthDate: birthDate,
+          },
+        ])
+        .execute();
+      return response;
+    };
+
+    const typeDefs = gql`
+      type Query {
+        hello: String
+      }
+      type User {
+        id: String
+        name: String
+        email: String
+        password: String
+        birthDate: String
+      }
+      type Mutation {
+        createUser(
+          id: String
+          name: String
+          email: String
+          password: String
+          birthDate: String
+        ): User!
+      }
+    `;
+
+    const resolvers = {
+      Query: {
+        hello: () => {
+          return "Hello world!";
+        },
+      },
+      Mutation: {
+        async createUser(parent, args) {
+          try {
+            const user = {
+              id: uuidv4(),
+              name: args.name,
+              email: args.email,
+              password: args.password,
+              birthDate: args.birthDate,
+            };
+
+            if (
+              !user.name ||
+              !user.email ||
+              !user.password ||
+              !user.birthDate
+            ) {
+              throw new Error("Please check the fields!");
+            }
+            console.log("user", user);
+            console.log(await addUser(user));
+
+            return { message: "User created" };
+          } catch (error: any) {
+            return `message: ${error.message}`;
+          }
+        },
+      },
+    };
+    const server = new ApolloServer({ typeDefs, resolvers });
+    server.listen(4000).then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
+  })
+  .catch((error) => console.log(error));
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,87 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { AppDataSource } from './data-source';
-import { User } from './entity/User';
-import { v4 as uuidv4 } from 'uuid';
+// import { ApolloServer, gql } from 'apollo-server';
+// import { AppDataSource } from './data-source';
+// import { User } from './entity/User';
+// import { v4 as uuidv4 } from 'uuid';
 
-AppDataSource.initialize()
-  .then(async () => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.name = 'Timber';
-    user.email = 'timber@email.com';
-    user.password = '123456';
-    user.birthDate = '01-01-1990';
-    await AppDataSource.manager.save(user);
-    console.log('Saved a new user');
-    console.log('Loading users from the database...');
-    const users = await AppDataSource.manager.find(User);
-    console.log('Loaded users: ', users);
+// AppDataSource.initialize()
+//   .then(async () => {
+//     console.log('Inserting a new user into the database...');
+//     const user = new User();
+//     user.name = 'Timber';
+//     user.email = 'timber@email.com';
+//     user.password = '123456';
+//     user.birthDate = '01-01-1990';
+//     await AppDataSource.manager.save(user);
+//     console.log('Saved a new user');
+//     console.log('Loading users from the database...');
+//     const users = await AppDataSource.manager.find(User);
+//     console.log('Loaded users: ', users);
 
-    console.log(
-      'Here you can setup and run express / fastify / any other framework.'
-    );
-  })
-  .catch((error) => console.log(error));
+//     console.log(
+//       'Here you can setup and run express / fastify / any other framework.'
+//     );
+//   })
+//   .catch((error) => console.log(error));
 
-//Data store mock
-const users = [
-  {
-    id: '123',
-    name: 'Robert',
-    email: 'robert@email.com',
-    password: 'crespusculo',
-    birthDate: '10-12-1986'
-  },
-  {
-    id: '321',
-    name: 'Kristen',
-    email: 'kristen@email.com',
-    password: 'crespusculo',
-    birthDate: '10-10-1985'
-  }
-];
+// //Data store mock
+// const users = [
+//   {
+//     id: '123',
+//     name: 'Robert',
+//     email: 'robert@email.com',
+//     password: 'crespusculo',
+//     birthDate: '10-12-1986'
+//   },
+//   {
+//     id: '321',
+//     name: 'Kristen',
+//     email: 'kristen@email.com',
+//     password: 'crespusculo',
+//     birthDate: '10-10-1985'
+//   }
+// ];
 
-//Type definition (schema)
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-  type User {
-    id: String
-    name: String
-    email: String
-    password: String
-    birthDate: String
-  }
-  type Mutation {
-    createUser(
-      id: String
-      name: String!
-      email: String!
-      password: String!
-      birthDate: String!
-    ): User!
-  }
-`;
+// //Type definition (schema)
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+//   type User {
+//     id: String
+//     name: String
+//     email: String
+//     password: String
+//     birthDate: String
+//   }
+//   type Mutation {
+//     createUser(
+//       id: String
+//       name: String!
+//       email: String!
+//       password: String!
+//       birthDate: String!
+//     ): User!
+//   }
+// `;
 
-const resolvers = {
-  Query: {
-    hello: () => {
-      return 'Hello world!';
-    }
-  },
-  Mutation: {
-    createUser(parent, args, ctx, info) {
-      const user = {
-        id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        password: args.password,
-        birthDate: args.birthDate
-      };
+// const resolvers = {
+//   Query: {
+//     hello: () => {
+//       return 'Hello world!';
+//     }
+//   },
+//   Mutation: {
+//     createUser(parent, args, ctx, info) {
+//       const user = {
+//         id: uuidv4(),
+//         name: args.name,
+//         email: args.email,
+//         password: args.password,
+//         birthDate: args.birthDate
+//       };
 
-      users.push(user);
-      return user;
-    }
-  }
-};
+//       users.push(user);
+//       return user;
+//     }
+//   }
+// };
