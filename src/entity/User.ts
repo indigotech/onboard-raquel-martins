5afbbafdcefca107d9a91465f0c7c39e7fbf89d2
Raquel Martins <85976494+Raquelmms@users.noreply.github.com>
2022-05-06 12:11:57 -0300

    alterando coluna primária

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,6 +3,9 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
   name: string;
 
   @Column()
