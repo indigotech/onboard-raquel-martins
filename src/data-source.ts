be93ff67edbcb2e1477c64bec61ae57f803ff4d7
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-09 09:41:32 -0300

    organizando arquivos

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,5 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
-import { v4 as uuidv4 } from 'uuid';
-import { ApolloServer, gql } from 'apollo-server';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -17,78 +14,3 @@ export const AppDataSource = new DataSource({
   migrations: [],
   subscribers: []
 });
-
-AppDataSource.initialize()
-  .then(() => {
-    const addUser = async ({ id, name, email, password, birthDate }) => {
-      const response = await AppDataSource.manager.insert(User, {
-        id,
-        name,
-        email,
-        password,
-        birthDate
-      });
-      return response;
-    };
-
-    const typeDefs = gql`
-      type Query {
-        hello: String
-      }
-      type User {
-        id: String
-        name: String
-        email: String
-        password: String
-        birthDate: String
-      }
-      type Mutation {
-        createUser(
-          id: String
-          name: String
-          email: String
-          password: String
-          birthDate: String
-        ): User!
-      }
-    `;
-
-    const resolvers = {
-      Query: {
-        hello: () => {
-          return 'Hello world!';
-        }
-      },
-      Mutation: {
-        async createUser(parent, args) {
-          try {
-            const user = {
-              id: uuidv4(),
-              name: args.name,
-              email: args.email,
-              password: args.password,
-              birthDate: args.birthDate
-            };
-
-            if (
-              !user.name ||
-              !user.email ||
-              !user.password ||
-              !user.birthDate
-            ) {
-              throw new Error('Please check the fields!');
-            }
-            const result = await addUser(user);
-            return result;
-          } catch (error: any) {
-            return `message: ${error.message}`;
-          }
-        }
-      }
-    };
-    const server = new ApolloServer({ typeDefs, resolvers });
-    server.listen(4000).then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`);
-    });
-  })
-  .catch((error) => console.log(error));
