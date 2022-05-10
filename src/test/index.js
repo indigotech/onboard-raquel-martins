288e213600723af62ca33eb47d309d3e0c0e514c
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-10 13:42:12 -0300

    teste

---
+++ src/test/index.js
@@ -0,0 +1,13 @@
+// import { setup } from './setup';
+// import { config } from 'dotenv';
+
+// before(async () => {
+//   config({ path: `${process.cwd()}/test.env` });
+//   await setup();
+// });
+
+describe('Test', () => {
+  it('should test', () => {
+    console.log('AAAAA');
+  });
+});
---
+++ src/test/test.ts
@@ -0,0 +1,7 @@
+describe('hello', function () {
+  describe('hello', function () {
+    it('should return hello', function () {
+      console.log('hello');
+    });
+  });
+});
