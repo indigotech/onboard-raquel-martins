e2347655521d9dfdca1f49d5791cecc4848d85f3
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-10 16:19:34 -0300

    testes

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,13 +1,5 @@
-// import { setup } from './setup';
-// import { config } from 'dotenv';
-
-// before(async () => {
-//   config({ path: `${process.cwd()}/test.env` });
-//   await setup();
-// });
-
-describe('Test', () => {
-  it('should test', () => {
-    console.log('AAAAA');
+describe('teste', () => {
+  it('should return hello message', async () => {
+    console.log('teste');
   });
 });
--- src/test/test.ts
+++
@@ -1,7 +0,0 @@
-describe('hello', function () {
-  describe('hello', function () {
-    it('should return hello', function () {
-      console.log('hello');
-    });
-  });
-});
