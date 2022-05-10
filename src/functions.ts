16447709700be6a835a0ac39ce0cb489c162a2bf
raquelmms <raqueldemedeirosmartins@gmail.com>
2022-05-10 09:01:45 -0300

    arrumando os returns

--- src/functions.ts
+++ src/functions.ts
@@ -4,21 +4,13 @@ import { User } from './entity/User';
 export const containLetter = (pass: string): boolean => {
   const letters = new RegExp('[A-Za-z]');
   const password = pass;
-  let findLetter = false;
-  if (letters.test(password)) {
-    findLetter = true;
-  }
-  return findLetter;
+  return letters.test(password);
 };
 
 export const containDigit = (pass: string) => {
   const digits = new RegExp('[0-9]+');
   const password = pass;
-  let findDigit = false;
-  if (digits.test(password)) {
-    findDigit = true;
-  }
-  return findDigit;
+  return digits.test(password);
 };
 
 export const findUserEmail = async (email: string): Promise<boolean> => {
@@ -26,9 +18,5 @@ export const findUserEmail = async (email: string): Promise<boolean> => {
   const findUser = await AppDataSource.manager.findOneBy(User, {
     email: userEmail
   });
-  let findEmail = false;
-  if (findUser !== null) {
-    findEmail = true;
-  }
-  return findEmail;
+  return !!findUser;
 };
